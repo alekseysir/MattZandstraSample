f67de589f41f0333544ba55494df28ffa29a889e
alekseysir <aleksey_sir@mail.ru>
2022-05-17 08:08:22 +0300

    method getProduser() is added

--- Classes and Objects.php
+++ Classes and Objects.php
@@ -5,6 +5,15 @@ class ShopProduct
     public $produserMainName = "main name";
     public $produserFirstName = "first name";
     public $price = 0;
+
+    public function getProduser()
+    {
+        return $this->produserFirstName." ".$this->produserMainName;
+    }
 }
 $product1 = new ShopProduct();
-print $product1->title;
+$product1->title = "My Antonia";
+$product1->produserMainName = "Cather";
+$product1->produserFirstName= "Willa";
+$product1->price = 5.99;
+print "Author: {$product1->getProduser()}";
